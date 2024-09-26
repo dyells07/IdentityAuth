2c92150fb08264310f35e75247ca294f63b242dc
dyells07 <bhimkhanal999@gmail.com>
2024-09-26 22:58:08 +0545

    struct : rdlc example

---
+++ RDLCExampleWebAPI/RdlcWebApi/Controllers/ReportController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RdlcWebApi.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Threading.Tasks;
+
+namespace RdlcWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReportController : ControllerBase
+    {
+        private IReportService _reportService;
+
+        public ReportController(IReportService reportService)
+        {
+            _reportService = reportService;
+        }
+
+        [HttpGet("{reportName}/{reportType}/{lang}")]
+        public ActionResult Get(string reportName, string reportType, string lang)
+        {
+            var reportNameWithLang = reportName + "_" + lang;
+            var reportFileByteString = _reportService.GenerateReportAsync(reportNameWithLang, reportType);
+            return File(reportFileByteString, MediaTypeNames.Application.Octet, getReportName(reportNameWithLang, reportType));
+        }
+
+
+        private string getReportName(string reportName, string reportType)
+        {
+            var outputFileName = reportName + ".pdf";
+
+            switch (reportType.ToUpper())
+            {
+                default:
+                case "PDF":
+                    outputFileName = reportName + ".pdf";
+                    break;
+                case "XLS":
+                    outputFileName = reportName + ".xls";
+                    break;
+                case "WORD":
+                    outputFileName = reportName + ".doc";
+                    break;
+            }
+
+            return outputFileName;
+        }
+
+    }
+}
