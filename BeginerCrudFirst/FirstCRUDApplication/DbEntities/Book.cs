5a173b359440d36ca39bc54c1bfc0d8c0f40dc6d
dyells07 <bhimkhanal999@gmail.com>
2024-10-09 19:09:05 +0545

    struct : crud

---
+++ BeginerCrudFirst/FirstCRUDApplication/DbEntities/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace FirstCRUDApplication.DbEntities
+{
+    public class Book:BaseEntity
+    {
+        public string Name { get; set; }
+        public string ISBN { get; set; }
+        public string Author { get; set; }
+        public string Publisher { get; set; }
+    }
+}
---
+++ BeginerCrudFirst/FirstCRUDApplication/DbEntities/BookMap.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FirstCRUDApplication.DbEntities
+{
+    public class BookMap 
+    {
+        public BookMap(EntityTypeBuilder<Book> entityBuilder)
+        {
+            entityBuilder.HasKey(t => t.Id);            
+            entityBuilder.Property(t => t.Name).IsRequired();
+            entityBuilder.Property(t => t.ISBN).IsRequired();
+            entityBuilder.Property(t => t.Author).IsRequired();
+            entityBuilder.Property(t => t.Publisher).IsRequired();           
+        }
+    }
+}
