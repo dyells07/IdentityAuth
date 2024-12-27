26bc2c040aefe28e243488c7022a7726af794e19
dyells07 <bhimkhanal999@gmail.com>
2024-12-27 22:20:01 +0545

    chore : minor changes

--- AngularProject/WebGYM/WebGYM/Startup.cs
+++ AngularProject/WebGYM/WebGYM/Startup.cs
@@ -36,7 +36,6 @@ namespace WebGYM
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.Configure<CookiePolicyOptions>(options =>
--- AngularProject/WebGYM/WebGYM/obj/Debug/netcoreapp2.1/WebGYM.AssemblyInfo.cs
+++ AngularProject/WebGYM/WebGYM/obj/Debug/netcoreapp2.1/WebGYM.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebGYM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc44b6eec5e5eb8093f3a4dc8c0d712e859a15cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9a4ca17651ffb476c525e0a2918be223e82a23b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebGYM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebGYM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
